7b18252dd9edbeaabcb6bd61541aaaa7013eff5f
Umair Khalid <umair.khalid786@outlook,.com>
2018-07-01 13:31:17 +0500

    Code changes

--- app/src/main/java/com/techlad/colorstrings_example/MainActivity.java
+++ app/src/main/java/com/techlad/colorstrings_example/MainActivity.java
@@ -12,23 +12,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView textView = (TextView) findViewById(R.id.attributed_tv);
 
 		List<ColoredString> coloredStringsList  = new ArrayList<>();
-
+		//Add create your colored String objects
 		ColoredString coloredString1 = new ColoredString(Color.BLACK , "Hello ");
 		ColoredString coloredString2 = new ColoredString(Color.RED , "Colored ");
 		ColoredString coloredString3 = new ColoredString(Color.BLACK , "World");
-
+		//add them to array of this objects
 		coloredStringsList.add(coloredString1);
 		coloredStringsList.add(coloredString2);
 		coloredStringsList.add(coloredString3);
-
+		//And then call this function to get SpannableStringBuilder
 		SpannableStringBuilder builder = StringPainter.getColoredText(coloredStringsList);
-
-		((TextView) findViewById(R.id.attributed_tv)).setText(builder , BufferType.SPANNABLE);
+		//Set this builder to textView without converting using toString() method on builder
+		textView.setText(builder , BufferType.SPANNABLE);
 	}
 }
